d42faedfc335f79361413b7e31bbcf02e1ffb827
Sierk Beij <sierk@riseup.net>
2019-01-13 22:13:51 -0800

    Removed lerna scaffolding from m8-scripts package.

--- packages/m8-scripts/__tests__/m8-scripts.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const m8Scripts = require('..');
-
-describe('@mutual8/m8-scripts', () => {
-    it('needs tests');
-});
--- packages/m8-scripts/lib/m8-scripts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = m8Scripts;
-
-function m8Scripts() {
-    // TODO
-}
