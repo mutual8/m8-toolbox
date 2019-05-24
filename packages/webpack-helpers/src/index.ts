6db1cd1e9c25769f3e8c3c4d694f181cbc2b0ec0
Sierk Beij <sierk@riseup.net>
2019-05-24 13:49:57 -0700

    Simplified @mutual8/webpack-helpers.

--- packages/webpack-helpers/src/index.ts
+++ packages/webpack-helpers/src/index.ts
@@ -1,14 +1,1 @@
-import { Compiler, Configuration, Plugin } from 'webpack'
-
-import { PluginFunction, PluginOptions } from './conditional-plugin-factory'
-
-// Export webpack types to so Mutual 8 modules do not need to depend on webpack directly.
-export type WebpackCompiler = Compiler
-export type WebpackConfiguration = Configuration
-export type WebpackPlugin = Plugin
-export type WebpackPluginFunction<T> = PluginFunction<T>
-export type WebpackPluginOptions<T> = PluginOptions<T>
-
 export { createConditionalWebpackPlugin } from './conditional-plugin-factory'
-export { HotModuleReplacementPlugin } from './plugins/hot-module-replacement'
-export { AggressiveMergingPlugin } from './plugins/aggressive-merging'
